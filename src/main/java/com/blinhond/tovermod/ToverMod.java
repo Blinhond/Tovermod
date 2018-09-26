dfc01fcc629e64774fc986fec231df54fb3b9cfc
Broodhond <borismurman@gmail.com>
2018-09-26 15:59:02 +0200

    Initial commit

---
+++ src/main/java/com/blinhond/tovermod/ToverMod.java
@@ -0,0 +1,30 @@
+package com.blinhond.tovermod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ToverMod.MODID, name = ToverMod.NAME, version = ToverMod.VERSION)
+public class ToverMod
+{
+    public static final String MODID = "btm";
+    public static final String NAME = "Tovermod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
